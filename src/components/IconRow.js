0bda51c425433fd67d21ea2eda81fca12d3d6feb
HippErger <berger.clayton@gmail.com>
2017-11-21 18:18:58 -0600

    styles added

--- src/components/IconRow.js
+++ src/components/IconRow.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import '../stylesheets/IconRow.css'
 
 function IconRow() {
   return (
-    <div>
+    <div class='iconDiv'>
       <a href='https://github.com/HippErger'>
-        <i class="fa fa-github fa-2x heady-Icon padded-Icon" aria-hidden="true" fa-3x></i>
+        <i class="fa fa-github fa-lg heady-Icon padded-Icon" aria-hidden="true" fa-3x></i>
       </a>
       <a href='https://www.linkedin.com/in/clayton-berger/'>
-        <i class="fa fa-linkedin-square fa-2x heady-Icon" aria-hidden="true"></i>
+        <i class="fa fa-linkedin-square fa-lg heady-Icon" aria-hidden="true"></i>
       </a>
       <a>
-        <i class="fa fa-twitter-square fa-2x heady-Icon" aria-hidden="true"></i>
+        <i class="fa fa-twitter-square fa-lg heady-Icon" aria-hidden="true"></i>
       </a>
+      <a href='#'>
+        <i class="fa fa-medium fa-lg heady-Icon" aria-hidden="true"></i>
+      </a>  
     </div>
   )
 }
