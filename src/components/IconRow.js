ac0cd07affde17098a3c1f8a62e312a8228531c1
HippErger <berger.clayton@gmail.com>
2017-11-29 17:05:11 -0600

    adjusted icon and font size after deployment

--- src/components/IconRow.js
+++ src/components/IconRow.js
@@ -5,20 +5,19 @@ function IconRow() {
   return (
     <div class='iconDiv'>
       <a href='https://github.com/HippErger'>
-        <i class="fa fa-github fa-lg heady-Icon padded-Icon" aria-hidden="true" fa-3x></i>
+        <i class="fa fa-github fa-2x heady-Icon padded-Icon" aria-hidden="true"></i>
       </a>
       <a href='https://www.linkedin.com/in/clayton-berger/'>
-        <i class="fa fa-linkedin-square fa-lg heady-Icon" aria-hidden="true"></i>
+        <i class="fa fa-linkedin-square fa-2x heady-Icon" aria-hidden="true"></i>
       </a>
       <a href='https://twitter.com/Berger_Clayton'>
-        <i class="fa fa-twitter-square fa-lg heady-Icon" aria-hidden="true"></i>
+        <i class="fa fa-twitter-square fa-2x heady-Icon" aria-hidden="true"></i>
       </a>
       <a href='https://medium.com/@berger.clayton'>
-        <i class="fa fa-medium fa-lg heady-Icon" aria-hidden="true"></i>
-      </a>  
+        <i class="fa fa-medium fa-2x heady-Icon" aria-hidden="true"></i>
+      </a>
     </div>
   )
 }
 
 export default IconRow;
-
